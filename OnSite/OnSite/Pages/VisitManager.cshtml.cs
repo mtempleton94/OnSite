4074b264476c3624202f2d799756dd1bbf8c8590
mtempleton94 <mitchelltempleton94@gmail.com>
2018-08-20 16:29:26 +0930

    Start of sidebar for selection of unapproved visits.

--- OnSite/OnSite/Pages/VisitManager.cshtml.cs
+++ OnSite/OnSite/Pages/VisitManager.cshtml.cs
@@ -22,19 +22,32 @@ namespace OnSite.Pages
         [BindProperty]
         public Visitor Visitor { get; set; }
 
+        [BindProperty]
+        public Organisation Organisation { get; set; }
+
         public IList<Visit> OnSiteVisitors { get; set; }
+        public IList<Visit> UnapprovedVisits { get; set; }
 
         //=====================================================================
         // Page Load - Get a list of all approved visits
         //=====================================================================
         public IActionResult OnGetAsync()
         {
+            // create list of approved visits
             IQueryable<Visit> onSiteVisitors =
                 from visit in _context.Visit
                 where visit.ApprovalStatus == true &&
                 visit.SignOutTime == null
                 select visit;
             OnSiteVisitors = onSiteVisitors.ToList();
+
+            // create list of unapproved visits
+            IQueryable<Visit> unapprovedVisits =
+                from visit in _context.Visit
+                where visit.ApprovalStatus == false
+                select visit;
+            UnapprovedVisits = unapprovedVisits.ToList();
+
             return Page();
         }
 
